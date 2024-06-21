fb4b420809418b8a7438c8c834c309e3582fed6f
Dylzzar <19xuerebd@gmail.com>
2024-06-21 21:46:01 +1000

    Started adding character

--- character.java
+++ character.java
@@ -7,8 +7,22 @@ public class Character {
 	private int curency;
 	
 	
-	public Character() {
-		health=5;
+	public Character(int choice) {
+		switch(choice){
+			case 1:
+				this.health = 20;
+				break;
+			case 2:
+				this.health = 15;
+				break;
+			case 3:
+				this.health = 10;
+				break;
+		}
+	}
+	
+	public int health() {
+		return this.health;
 	}
 	
 }
